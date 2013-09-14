2bfaf658601045bd43910422d8ce9fccf04c9eda
Filip Procházka <filip@prochazka.su>
2013-09-14 03:45:18 +0200

    Day and Second

---
+++ src/Kdyby/Clock/Precision/Second.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\Clock\Precision;
+
+use DateTimeZone;
+use Kdyby;
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class Second extends Kdyby\Clock\TimeFrame implements Kdyby\Clock\DateTime
+{
+
+	public function __construct($time = 'now', DateTimeZone $timezone = null)
+	{
+		parent::__construct('Y-m-d H:i:s', $time, $timezone);
+	}
+
+}
